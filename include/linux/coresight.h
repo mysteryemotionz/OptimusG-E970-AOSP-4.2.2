36a6328b3771410fee1cf61a61e12cb97ca8bacc
houstonn <houstonn@gmail.com>
2013-04-25 21:30:19 +0000

    fix warning: hw_breakpoint @chad

--- include/linux/coresight.h
+++ include/linux/coresight.h
@@ -34,7 +34,7 @@
 #define ARM_DEBUG_ARCH_V7B	(0x3)
 /* DBGv7 with all CP14 registers implemented */
 #define ARM_DEBUG_ARCH_V7	(0x4)
-#define ARM_DEBUG_ARCH_V7_1	(0x5)
+/* #define ARM_DEBUG_ARCH_V7_1	(0x5) */
 #define ETM_ARCH_V3_3		(0x23)
 #define PFT_ARCH_V1_1		(0x31)
 
